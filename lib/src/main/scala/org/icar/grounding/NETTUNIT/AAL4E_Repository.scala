806b1e2ca53d032a2887e3db62214887abe6ccf7
Davide Andrea Guastella <davide.guastella@icar.cnr.it>
2022-09-19 14:21:46 +0200

    Added AAL4E concrete capability repository
    Added function to deploy and execute workflows from MUSA

---
+++ lib/src/main/scala/org/icar/grounding/NETTUNIT/AAL4E_Repository.scala
@@ -0,0 +1,28 @@
+package org.icar.grounding.NETTUNIT
+
+import org.icar.grounding.{CapabilityRepository, ConcreteCapability}
+import org.icar.symbolic.AbstractCapability
+
+import scala.annotation.tailrec
+
+case class AAL4E_Repository(capabilities: List[AbstractCapability]) extends CapabilityRepository {
+
+  @tailrec
+  final def aux(capabilities: List[AbstractCapability]): Unit = capabilities match {
+    case (cap: AbstractCapability) :: tail => {
+      for (typeID <- 1 to 10) {
+        add(cap.id, // -> the service name
+          ConcreteCapability(typeID,
+            cap.id, // -> the service name (here we are in a concrete capability)
+            s"aal4e.handler.${cap.id}_type${typeID}", // -> the class that is used to realize the service
+            Some(s"aal4e.listener.TaskStartedExecutionListenerImpl"),
+            Some(s"aal4e.listener.TaskEndendExecutionListenerImpl"))) // -> the listener executed when the BPMN task associated to this capability begins its execution
+      }
+      aux(tail)
+    }
+    case _ :: _ =>
+    case Nil =>
+  }
+
+  aux(capabilities)
+}
--- lib/src/main/scala/org/icar/pmr_solver/Test_BestFirst_Solver.scala
+++ lib/src/main/scala/org/icar/pmr_solver/Test_BestFirst_Solver.scala
@@ -1,17 +1,24 @@
 package org.icar.pmr_solver
 
-import org.icar.GoalSPECParser.Goal2BPMN
-import org.icar.grounding.NETTUNIT.{NETTUNITProcessDecoratorStrategy, Test_BestFirstSolver_Repository}
+import org.icar.GoalSPECParser.{Goal2BPMN, testParserImpl}
+import org.icar.GoalSPECParser.testParserImpl.demo_goal
+import org.icar.bpmn2goal.bpmn_parser
+import org.icar.grounding.NETTUNIT.{AAL4E_Repository, NETTUNITProcessDecoratorStrategy, Test_BestFirstSolver_Repository}
 import org.icar.grounding.SolutionGrounder
 import org.icar.grounding.groundingStrategy.TabuGroundingStrategy
 import org.icar.pmr_solver.best_first_planner.{Solver, WTS2Solution}
 import org.icar.pmr_solver.planning_domain.{AAL4E_cognitive_stimulation, GenericPlannerExecutor, IDS_like_domain, RandomlyGeneratedDomain}
+import org.icar.service.BPMN2GoalSPEC
 import org.icar.sublevel.{HL2Raw_Map, RawState}
 import org.icar.symbolic.{Domain, Problem}
+import scalaj.http.{Http, HttpOptions}
 
+import java.io.{ByteArrayInputStream, InputStream}
 import scala.Console.{BLACK_B, BOLD, RESET, YELLOW}
 
 object Test_Solver_AAL4E extends App {
+  val bpmnProcessID = "AAL4EProcess"
+
   val my_domain = AAL4E_cognitive_stimulation.my_domain
   val my_problem = AAL4E_cognitive_stimulation.my_problem
   val map = AAL4E_cognitive_stimulation.map
@@ -37,11 +44,17 @@ object Test_Solver_AAL4E extends App {
       print("wts size= " + wts.nodes.size)
       println("=> time = " + total_time)
 
-      val capabilityRepository = Test_BestFirstSolver_Repository(my_problem.actions.sys_action.toList)
+      //val list_of_goals = testParserImpl.demo_goal()
+
+      val capabilityRepository = AAL4E_Repository(my_problem.actions.sys_action.toList)
       val grounder = new SolutionGrounder(capabilityRepository, new TabuGroundingStrategy(2))
       grounder.setProcessDecorator(NETTUNITProcessDecoratorStrategy)
       val solution = grounder.groundSolution(converter)
-      val theBPMN = Goal2BPMN.getBPMN(solution, "myBPMNProcess", "process_0")
+      val theBPMN = Goal2BPMN.getBPMN(solution, "myBPMNProcess", bpmnProcessID)
+
+      val is = new ByteArrayInputStream(theBPMN.toString().getBytes)
+      val parser = new bpmn_parser(is)
+      val goalString = parser.fullFromInputStream
 
       Console.out.println(s"${RESET}${BLACK_B}${YELLOW}${BOLD}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${RESET}")
       Console.out.println(s"${RESET}${BLACK_B}${YELLOW}${BOLD}BPMN PROCESS!${RESET}")
@@ -49,7 +62,26 @@ object Test_Solver_AAL4E extends App {
       Console.out.println(theBPMN.toString())
       Console.out.println(s"${RESET}${BLACK_B}${YELLOW}${BOLD}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${RESET}")
 
+      executeWithFlowable(theBPMN.toString())
+
     }
+  }
+
+  def executeWithFlowable(processDef: String): Unit = {
+    Console.out.println(s"${RESET}${BLACK_B}${YELLOW}${BOLD}DEPLOYING BPMN PROCESS${RESET}")
+    //replace tee with a dummy variable...
+    val teeSymbol = "\u22A4"
+    val repl = "${myVariable}"
+    val newProcessDef = processDef.replace(teeSymbol, repl)
+    val resultDeploy = Http(s"http://localhost:8080/AAL4E/deployProcess/${bpmnProcessID}")
+      .postData(newProcessDef)
+      .header("Content-Type", "application/xml").asString
+
+    Console.out.println(s"${RESET}${BLACK_B}${YELLOW}${BOLD}EXECUTING BPMN PROCESS${RESET}")
+    val requestBody = s"{\n  \"processID\":\"$bpmnProcessID\",\n  \"requestDescription\":\"ciao\"\n}"
+    val resultApply = Http(s"http://localhost:8080/AAL4E/apply")
+      .postData(requestBody)
+      .header("Content-Type", "application/json").asString
   }
 
 }
@@ -104,7 +136,6 @@ object Test_Solver_Random extends App {
         val total_time: Long = end_time - start_time
         print("wts size= " + wts.nodes.size)
         println("=> time = " + total_time)
-
 
         val capabilityRepository = Test_BestFirstSolver_Repository(my_problem.actions.sys_action.toList)
         val grounder = new SolutionGrounder(capabilityRepository, new TabuGroundingStrategy(2))
